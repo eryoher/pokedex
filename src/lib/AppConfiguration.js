6a54baf2772cac9bcca33615000cf329259e98bd
Juan Farina <jfarina@agminen.com>
2019-05-26 12:01:07 -0300

    added TODO

--- src/lib/AppConfiguration.js
+++ src/lib/AppConfiguration.js
@@ -1,3 +1,4 @@
+//TODO jfarina implement: should load config from a JS/JSON file as first thing, then load the rest of the app
 export default function getAppConfiguration() {
 
     const config = {

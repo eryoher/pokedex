b7095e8564116cbe6ac7e2cc91795f27cdfc9672
Juan Farina <jfarina@agminen.com>
2019-07-07 19:14:58 -0300

    #2167 - Adjusted mock loading conditions

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import 'lib/AxiosInterceptors'
-import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'nprogress/nprogress.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import "lib/AxiosInterceptors";
+import * as serviceWorker from "./serviceWorker";
+import "bootstrap/dist/css/bootstrap.css";
+import "nprogress/nprogress.css";
 
-if (process.env.NODE_ENV === 'development') {
-  import('__mocks__').then( () => {
-    ReactDOM.render(<App />, document.getElementById('root'));
-  })
-} else {
-  ReactDOM.render(<App />, document.getElementById('root'));
-}
+const loadApp = async () => {
+  if (process.env.REACT_APP_MOCK_ENABLED) {
+    await import("__mocks__");
+  }
+  ReactDOM.render(<App />, document.getElementById("root"));
 
+  // If you want your app to work offline and load faster, you can change
+  // unregister() to register() below. Note this comes with some pitfalls.
+  // Learn more about service workers: http://bit.ly/CRA-PWA
+  serviceWorker.unregister();
+};
 
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+loadApp();

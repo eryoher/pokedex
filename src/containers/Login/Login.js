1aeb912d2f55285f53b51214dc16892eda723ab1
eryoher <eryoher@gmail.com>
2019-07-23 12:10:15 -0500

    update logo login

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,8 +1,8 @@
 // @flow
 import React, { Component } from 'react';
-import {Row, Col, Image} from 'react-bootstrap';
+import { Row, Col, Image } from 'react-bootstrap';
 import LoginForm from 'components/login/loginForm';
-import {Card} from 'reactstrap';
+import { Card } from 'reactstrap';
 //import logo from '../../../public/assets/images/login_logo.png';
 
 
@@ -13,23 +13,23 @@ type Props = {
 export default class Login extends Component<Props> {
   render() {
     const { theme } = this.props;
-    
+
     return (
-        < >
-          <div className={"container"} >
-            <Col sm={12} className={`mt-5 ${theme.containerLogin}`}>
-              <Card className={'pt-4 pb-2'} style={{width:'70%', margin:'0px auto'}}>
-                <Col className={"text-center"}>
-                    <Image className={theme.imgLogo} src="assets/images/logo.svg" />
+      < >
+        <div className={"container"} >
+          <Col sm={12} className={`mt-5 ${theme.containerLogin}`}>
+            <Card className={'pt-4 pb-2'} style={{ width: '70%', margin: '0px auto' }}>
+              <Col className={"text-center"}>
+                <Image className={theme.imgLogo} src="assets/images/login_logo.png" />
+              </Col>
+              <LoginForm />
+              <Col className={"pt-5 text-center"} >
+                www.carenasistemas.com.ar
                 </Col>
-                <LoginForm />
-                <Col className={"pt-5 text-center"} >
-                    www.carenasistemas.com.ar
-                </Col>
-              </Card> 
-            </Col>
-          </div>
-        </>
+            </Card>
+          </Col>
+        </div>
+      </>
     )
   }
 }
